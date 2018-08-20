9b4be5f2751359cef7bec280c109f81365d4ef3f
hongqingWang <272338444@qq.com>
2018-08-20 20:28:33 +0800

    初始化代码
    初始化代码

---
+++ XiMaLaShi/XiMaLaShi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiMaLaShi
+//
+//  Created by 王红庆 on 2018/8/20.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
